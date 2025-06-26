15f71608f9d6ed7bb1c6bf96c307fdb707b1f952
juanlopezmarin <Juanteniente45@gmail.com>
2025-06-26 14:08:05 +0200

    1

---
+++ BeimanFederados/BeimanFederadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeimanFederadosApp.swift
+//  BeimanFederados
+//
+//  Created by Juan López Marín on 26/6/25.
+//
+
+import SwiftUI
+
+@main
+struct BeimanFederadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView() // o PaginaPrincipalViewController() si ya has renombrado la vista principal
+        }
+    }
+}
